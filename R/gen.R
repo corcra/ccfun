286b0c7e1401b5020907c44e166c845f458c02fe
Steve Harris <git@steveharris.me>
2016-05-21 00:30:40 +0100

    fixes

--- R/gen.R
+++ R/gen.R
@@ -1,7 +1,5 @@
 #' Various helper functions that GENerate derived variables
 #'
-#' gen_ppv makes a logical vector if positive pressure ventilated
-#' gen_map derives MAP
 
 
 #  =================================
@@ -9,6 +7,8 @@
 #  =================================
 #' @examples
 #' # gen_ppv(ddata, time, id, Total_Resp_Rate_Ventil)
+
+#' gen_ppv makes a logical vector if positive pressure ventilated
 gen_ppv <- function(dt, t_=time, id_= id, rrate_ppv_) {
     # - [ ] TODO(2016-05-20): add in airway and ventilated fields 
 
@@ -25,13 +25,14 @@ gen_ppv <- function(dt, t_=time, id_= id, rrate_ppv_) {
     # plus time
     # plus a series of columns or logical expression that can be used to define PPV
 
-    setkeyv(ddata, c(id_, t_))
-    ddata[, `:=`(ppv =NULL)]
-    ddata[, `:=`(
-        ppv = get(rrate_ppv_) > 0,
+    setkeyv(dt, c(id_, t_))
+    if ("ppv" %chin% names(dt)) dt[, `:=`(ppv =NULL)]
+    dt[, `:=`(
+        ppv = get(rrate_ppv_) > 0
+        ),
         roll=+Inf, # roll forwards without limit
         rollends=c(FALSE, TRUE) # roll forwards from the last value, but not back from first
-                 )]
+                 ]
 }
 
 #  ==========================
@@ -40,12 +41,14 @@ gen_ppv <- function(dt, t_=time, id_= id, rrate_ppv_) {
 # - [ ] FIXME(2016-05-20): wrap these two functions into one (gen_map and choose_first_nonmissing)
 
 #' Generate MAP from blood pressure
+#' gen_map derives MAP
 gen_map <- function(bps, bpd) {
     return(bpd + (bps-bpd)/3)
 }
 
-#' Choose a 'working' MAP by prioritising invasive over non-invasive and measured over calculated
+#' Choose first non-missing
 choose_first_nonmissing <- function(colvalues) {
+    library(purrr)
     reduce(colvalues, function(x,y) ifelse(!is.na(x), x, y))
 }
 
