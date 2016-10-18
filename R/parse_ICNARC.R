1ad0c9d7baefca00f3d882ccc09548c5b088f021
Steve Harris <git@steveharris.me>
2016-10-18 22:43:10 +0100

    added ICNARC parser

---
+++ R/parse_ICNARC.R
@@ -0,0 +1,111 @@
+#' @title Parses ICNARC diagnostic codes
+#'
+#' @description 
+#' Split text codes by level and then label as per ICNARC codes
+#'
+#' @import data.table
+#' @param dt data.table containing diagnostic codes to be parsed
+#' @param dc.col column name within data table with diagnostic codes 
+#' @param prefix new diagnostic component column prefix after parsing
+#'
+
+#' @export
+#' Split ICNARC text code, and convert to numeric
+split.icnarc.code <- function(dt, dc.col, prefix=NULL) {
+    # Split each code into components and convert to numeric to handle padding
+    if (is.null(prefix)) {
+        prefix <- "dc"
+    } 
+    dc.labels <- paste(prefix, seq(5), sep=".")
+    dt[, (dc.labels) := tstrsplit(get(dc.col), "\\.")]
+    dt[, (dc.labels) := lapply(.SD, as.numeric), .SDcols=dc.labels]
+    # return(dt)
+}
+
+#' @export
+#' Split and label ICNARC diagnostic code
+parse.icnarc.code <- function(dt, dc.col, prefix=NULL) {
+  icnarc.dc <- icnarc.dc.load()
+  icnarc.dc <- split.icnarc.code(icnarc.dc, "icnarc.dc")
+  setkeyv(icnarc.dc, paste0("dc.", seq(5)) )
+  split.icnarc.code(dt, dc.col)
+  setkeyv(dt, paste0("dc.", seq(5)) )
+  dt <- icnarc.dc[dt]
+
+  if (!is.null(prefix)) {
+    print(paste0(prefix, seq(5)))
+    setnames(dt, paste0("dc.", seq(5)), paste0(prefix, seq(5)) )
+  } 
+
+  return(dt)
+}
+# rm(tdt)
+# wdt[,dc1.2 := NULL]
+# names(wdt)
+# tdt <- copy(wdt)
+# tdt <- parse.icnarc.code(tdt, "diag_admx1", prefix="foo")
+# names(tdt)
+
+# icnarc.dc
+# str(icnarc.dc)
+
+# lookfor(diag, wdt)
+# # Work just with primary reason for admission
+
+# icnarc.dc.organs(tdt, dc.2.col="dc.2")
+# tdt[, lapply(.SD, head, 1), by=id, .SDcols=c("condition", "dc.2")]
+
+
+# Load ICNARC diagnostic code info
+# helper function
+icnarc.dc.load <- function() {
+    path <- system.file("data", "icnarc_codes.csv", package="ccfun") 
+    icnarc.dc <- read.csv(path, stringsAsFactors=FALSE)
+    setDT(icnarc.dc)
+    icnarc.dc <- subset(icnarc.dc, Condition!="")
+    icnarc.dc <- data.table::melt(icnarc.dc, id.vars=c("Condition") )
+    setnames(icnarc.dc, "value", "icnarc.dc")
+    setnames(icnarc.dc, "variable", "adm.type")
+    setnames(icnarc.dc, "Condition", "condition")
+    setkey(icnarc.dc, icnarc.dc)
+    return(icnarc.dc)
+}
+
+
+#' @export
+#' Label up ICNARC surgical level code
+label.icnarc.dc1 <- function(dt, dc.1.col="dc.1") {
+    # Add organ coding labels # as factor labels
+    # 1 - A condition where surgery has been performed (Surgical code)
+    # 2 - A condition where no surgery has been performed (non-surgical code) 
+    i <- data.table(
+        llabel = c(
+            "surgical condition",
+            "non-surgical condition"
+            ), 
+        dc.1 = c(1,2))
+    dt[, (dc.1.col) := factor(get(dc.1.col), levels=i$dc.1, labels=i$llabel)]
+}
+
+#' @export
+#' Label up ICNARC organ level code
+label.icnarc.dc2 <- function(dt, dc.2.col="dc.2") {
+    # Add organ coding labels # as factor labels
+    i <- data.table(
+        llabel = c(
+            "Respiratory",
+            "Cardiovascular",
+            "Gastrointestinal",
+            "Neurological (including eyes)",
+            "Poisoning",
+            "Genito-urinary",
+            "Endocrine, Metabolic, Thermoregulation and Poisoning",
+            "Haematological/Immunological",
+            "Musculoskeletal",
+            "Dermatological",
+            "Psychiatric",
+            "Trauma" ), 
+        # Order is as per coding (there is no 5)
+        dc.2 = c(1,2,3,4,6,7,8,9,10,11,12,13))
+    dt[, (dc.2.col) := factor(get(dc.2.col), levels=i$dc.2, labels=i$llabel)]
+}
---
+++ tests/testthat/test_icnarc.R
@@ -0,0 +1,21 @@
+context("Testing ICNARC parsing functions")
+
+# First arg = description of function
+# Subsequent args = function for testing e.g. expect_equal
+
+test_that("Check DKA", {
+    d <- data.table(code="2.8.4.10.1", diag="Diabetic ketoacidosis")
+    d <- split.icnarc.code(d, "code")
+    expect_equivalent(
+        c(2,8,4,10,1),
+        as.numeric(d[,.(dc.1,dc.2,dc.3,dc.4,dc.5)]))
+    d <- parse.icnarc.code(d, "code")
+    expect_equivalent(d$condition, d$diag)
+    d <- label.icnarc.dc1(d)
+    expect_equivalent(as.character(d$dc.1), "non-surgical condition")
+    d <- label.icnarc.dc2(d)
+    expect_equivalent(as.character(d$dc.2), "Endocrine, Metabolic, Thermoregulation and Poisoning")
+    # label.icnarc.dc1(d)
+
+})
+
